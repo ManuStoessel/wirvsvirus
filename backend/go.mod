275913a975fb0600313572855051e61fd3c0096e
Alexander Wagner <info@alexanderwagner.eu>
2020-03-21 13:18:35 +0100

    start gorm implementation

--- backend/go.mod
+++ backend/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.7.4
 	github.com/gorilla/sessions v1.2.0 // indirect
+	github.com/jinzhu/gorm v1.9.12
 	github.com/okta/okta-sdk-golang v1.1.0 // indirect
 )
--- backend/user/user.go
+++ backend/user/user.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 // User type
@@ -11,26 +13,49 @@ type User struct {
 	Email    string
 }
 
+var db *gorm.DB
+
+// Question: The db should be closed some time, or not?
+func init() {
+	if db == nil {
+		// TODO: Should be a one liner
+		dbobject, err := gorm.Open("mysql", "root:my-secret-pw@/wirvsvirus?charset=utf8&parseTime=True&loc=Local")
+		db = dbobject
+
+		if err != nil {
+			panic("failed to connect database")
+		}
+
+		db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(&User{})
+	}
+}
+
 // Create a user
 func Create(user *User) (*User, error) {
 	if user.ID == "" {
 		user.ID = uuid.New().String()
 	}
 
-	// Persist
+	db.Create(&user)
+
+	return user, nil
 }
 
 // Read get user by id
-func Read(id string) (*User, error) {
-	return nil, nil
+func Read(id string) (user *User, err error) {
+	user = &User{}
+
+	db.Where("id = ?", id).First(&user)
+
+	return
 }
 
 // Update a user
 func Update(user *User) (*User, error) {
-
+	return nil, nil
 }
 
 // Delete a user
 func Delete(user *User) error {
-
+	return nil
 }
